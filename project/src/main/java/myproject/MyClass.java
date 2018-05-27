8e3f46a7a839bcba8130dd6d9101b4a5e1a25612
Saber Seddik <seddik.saber@gmail.com>
2018-05-27 09:52:08 -0400

    Ivy repo

---
+++ project/src/main/java/myproject/MyClass.java
@@ -0,0 +1,9 @@
+package myproject;
+
+import someOrganization.someModule.SomeClass;
+
+public class MyClass {
+    public static void main(String[] args) {
+        SomeClass.someAction();
+    }
+}
